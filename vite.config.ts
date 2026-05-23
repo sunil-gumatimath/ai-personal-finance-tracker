e2445af12a46b90babebaa68c8d497d7dbaa648e
SuniL <sunilgumatimath.vs@gmail.com>
2026-05-23 14:49:18 +0530

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -18,14 +18,14 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "pwa-192x192.svg",
             sizes: "192x192",
-            type: "image/png",
+            type: "image/svg+xml",
           },
           {
-            src: "pwa-512x512.png",
+            src: "pwa-512x512.svg",
             sizes: "512x512",
-            type: "image/png",
+            type: "image/svg+xml",
           },
         ],
       },
